dd2986a7499caadd9856e43523e07555c4d7b25e
Jasandeep Singh <jasandeep.singh.nga@crowdbotics.com>
2023-07-07 11:40:29 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "nameless-meadow-41912",
   url: "https://nameless-meadow-41912.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,10 +1,13 @@
-import React from "react"
-import { View, Image, Text, ScrollView, SafeAreaView } from "react-native"
-import { styles } from "./styles"
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { StyleSheet } from "react-native";
+import React from "react";
+import { View, Image, Text, ScrollView, SafeAreaView } from "react-native";
+import { styles } from "./styles";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  const navigation = useNavigation();
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
         <View style={styles.group}>
@@ -14,9 +17,33 @@ const WelcomeScreen = () => {
           </Text>
         </View>
         <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+      <Pressable><View style={_styles.VfntfKUA}><Pressable onPress={() => {
+            navigation.navigate("login2");
+          }}><Text style={_styles.hnSDFxaK}>{"Get Started"}</Text></Pressable></View></Pressable></ScrollView>
+    </SafeAreaView>;
+};
 
-export default WelcomeScreen
+export default WelcomeScreen;
+
+const _styles = StyleSheet.create({
+  VfntfKUA: {
+    height: 55,
+    width: 148,
+    backgroundColor: "#fc8888",
+    borderRadius: 15,
+    color: "#777777"
+  },
+  hnSDFxaK: {
+    width: 141,
+    height: 49,
+    lineHeight: 50,
+    fontSize: 20,
+    borderRadius: 0,
+    textAlign: "center",
+    fontWeight: "700",
+    position: "absolute",
+    left: 5,
+    top: 2,
+    opacity: 0.55
+  }
+});
