faf7cf0559434295bdff8dd3e64784dd5f86c862
Jasandeep Singh <jasandeep.singh.nga@crowdbotics.com>
2023-07-12 12:33:17 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,3 +1,4 @@
+import { Pressable } from "react-native";
 import { View } from "react-native";
 import { Text } from "react-native";
 import React from "react";
@@ -5,22 +6,26 @@ import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled4 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}><View style={styles.SimzJjpb}><Text style={styles.qoopcjXP}>Lorem ipsum…</Text></View></ScrollView>
+    }}>
+        <View style={styles.SimzJjpb}>
+          <Text style={styles.qoopcjXP}>Lorem ipsum…</Text>
+        <Pressable><View style={styles.HzySHJlq}></View></Pressable><Pressable><View style={styles.OTnqDYxP}></View></Pressable></View>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
   },
   SimzJjpb: {
-    height: 632,
-    width: 382,
+    height: "100%",
+    width: "100%",
     backgroundColor: "#fcaaaa",
     borderRadius: 0,
     color: "#777777",
@@ -37,6 +42,26 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 291,
     left: 141
+  },
+  HzySHJlq: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#e56c6c",
+    borderRadius: 10,
+    color: "#777777",
+    position: "absolute",
+    left: 29,
+    top: 487
+  },
+  OTnqDYxP: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E56C6C",
+    borderRadius: 10,
+    color: "#777777",
+    position: "absolute",
+    left: 190,
+    top: 487
   }
 });
 export default Untitled4;
