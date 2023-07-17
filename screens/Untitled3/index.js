3fc739291a69e9ace78e4e6322fb0d2b18660a8c
Jasandeep Singh <jasandeep.singh.nga@crowdbotics.com>
2023-07-17 12:31:26 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled3 = () => {
   return <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={{
-      backgroundColor: "#f0f0f1",
+      backgroundColor: "purple",
       padding: 10,
       position: "relative",
       flex: 1
-    }}>
-        <View style={styles.zVYFbZKa}></View>
-      </ScrollView>
+    }}></ScrollView>
     </SafeAreaView>;
 };
 
@@ -18,19 +16,6 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     height: "100%"
-  },
-  zVYFbZKa: {
-    flex: 1,
-    height: "100%",
-    width: "100%",
-    backgroundColor: "purple",
-    borderRadius: 0,
-    color: "#777777",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
   }
 });
 export default Untitled3;
