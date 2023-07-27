7895a5d0c03c9668e5c3f65bd99df4b126983a12
Jasandeep Singh <jasandeep.singh.nga@crowdbotics.com>
2023-07-27 11:52:29 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,17 +1,24 @@
 import { useNavigation } from "@react-navigation/native";
-import React from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity, SafeAreaView, StyleSheet } from "react-native";
 
 const Screen = () => {
   const navigation = useNavigation();
   return <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.welcomeText}>Welcome!</Text>
-        <TouchableOpacity style={styles.button} onPress={() => {
-        navigation.navigate("accountSettings");
-      }}>
-          <Text style={styles.buttonText}>Proceed</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.button, styles.proceedButton]} onPress={() => {
+          navigation.navigate("accountSettings");
+        }}>
+            <Text style={styles.buttonText}>Proceed</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.backButton]} onPress={() => {
+          navigation.goBack();
+        }}>
+            <Text style={styles.buttonText}>Back</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>;
 };
@@ -19,28 +26,43 @@ const Screen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff"
   },
   content: {
-    alignItems: 'center'
+    alignItems: "center"
   },
   welcomeText: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20
   },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%"
+  },
   button: {
-    backgroundColor: '#007AFF',
+    flex: 1,
     paddingVertical: 10,
     paddingHorizontal: 20,
-    borderRadius: 5
+    borderRadius: 5,
+    marginBottom: 10
+  },
+  proceedButton: {
+    backgroundColor: "#007AFF",
+    marginRight: 10
+  },
+  backButton: {
+    backgroundColor: "#FF3B30",
+    marginLeft: 10
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: "bold",
+    textAlign: "center"
   }
 });
 export default Screen;
